e2754dc2b1a9e4cb6536b0c0f0708ece01968d0f
wei du <weidu@weis-Mac-mini.hsd1.va.comcast.net>
2019-05-14 16:15:07 -0400

    new dependencies added, routes working as planned

--- middelware/restricted.js
+++ middelware/restricted.js
@@ -1,22 +1,25 @@
 
-const bcrypt = require('bcryptjs');
-const db = require('../data/dbConfig');
-const Users = require('../users/users-model');
-
-
-
 function restrictions(req, res, next){
-    const{ username, password} = req.headers;
-    if(username&&password){
-        Users.findBy({username})
-        .first()
-        .then(user => {
-            user&&bcrypt.compareSync(password, user.password)?next(): res.status(401).json({message:"not the right creds"})
-        })
-    }else{
-        res.status(404).json({message:"incomplete creds"})
-    }
+ req.session&&req.session.user ? next() : res.status(401).json({ message:'additional info needed'})
 }
 
 
 module.exports = restrictions;
+
+
+// below are the original code (inside the function above) for the middleware, however require imports:
+//const bcrypt = require('bcryptjs'); <-- this will link the bcrypt
+//const db = require('../data/dbConfig'); <-- this will get the data from migration
+//const Users = require('../users/users-model'); <-- this will get user data functions
+
+
+// const{ username, password} = req.headers;
+//     if(username&&password){
+//         Users.findBy({username})
+//         .first()
+//         .then(user => {
+//             user&&bcrypt.compareSync(password, user.password)?next(): res.status(401).json({message:"not the right creds"})
+//         })
+//     }else{
+//         res.status(404).json({message:"incomplete creds"})
+//     }
